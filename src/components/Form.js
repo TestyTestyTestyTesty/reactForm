3c6b796dd71b220f68c8e52d59b86c20023ebebc
STProjectPeople <tomasz.sikora@projectpeople.pl>
2022-02-17 20:29:37 +0100

    form validation added

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,45 +1,97 @@
-import { useState } from "react";
-import {StyledForm,StyledRow, StyledLabel, StyledInput, StyledSubmit} from './styles/form'
-export default function Form() {
-    const [form,setForm] = useState({
-        firstName: '',
-        lastName: '',
-        pesel: '',
-        phone: '',
-        password:'',
-    });
+import { useEffect } from "react";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import {
+  StyledForm,
+  StyledRow,
+  StyledLabel,
+  StyledInput,
+  StyledSubmit,
+  StyledError
+} from "./styles/form";
+import { yupResolver } from "@hookform/resolvers/yup";
 
-    
-    const handleFormChange = (e) => {
-        setForm({
-            ...form,
-            [e.target.name]: e.target.value
-        });
-    }
-    const handleSubmit = () => {
-        console.log(form);
-    }
+let schema = yup.object().shape({
+  firstName: yup.string().required('First name is required'),
+  lastName: yup.string().required('Last name is required'),
+  pesel: yup.string().required('PESEL is required').test('len', 'PESEL must be exactly 11 characters', val => val.length === 11),
+  password: yup.string().min(6).max(20).required('Password is required'),
+});
+
+export default function Form() {
+
+  const { register, handleSubmit, formState} = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const submitForm = (data) => {
+    console.log(data);
+  };
+
+/*   useEffect(() => {
+    console.log( formState);
+  },[formState]); */
   return (
-    <StyledForm onSubmit={handleSubmit}>
-        <StyledRow>
-            <StyledLabel htmlFor="firstName">First name:</StyledLabel>
-            <StyledInput type="text" name="firstName" id="first-name" placeholder="First name" onChange={handleFormChange} value={form.firstName}/>
-        </StyledRow>
-        <StyledRow>
-            <StyledLabel htmlFor="lastName">Last name:</StyledLabel>
-            <StyledInput type="text" name="lastName" id="last-name" placeholder="Last name" onChange={handleFormChange} value={form.lastName}/>
-        </StyledRow>
-        <StyledRow>
-            <StyledLabel htmlFor="pesel">Last name:</StyledLabel>
-            <StyledInput type="number" name="pesel" id="pesel" placeholder="PESEL" onChange={handleFormChange} value={form.pesel}/>
-        </StyledRow>
-        <StyledRow>
-            <StyledLabel htmlFor="password">Last name:</StyledLabel>
-            <StyledInput type="password" name="password" id="password" placeholder="Password" onChange={handleFormChange} value={form.pesel}/>
-        </StyledRow>
-        <StyledRow>
-            <StyledSubmit class="form__submit" type="submit" value="Submit"/>
-        </StyledRow>
+    <StyledForm onSubmit={handleSubmit(submitForm)}>
+      <StyledRow>
+        <StyledLabel htmlFor="firstName">First name:</StyledLabel>
+        <StyledInput
+          type="text"
+          name="firstName"
+          id="first-name"
+          placeholder="First name"
+          error={formState.errors.firstName}
+          {...register("firstName", {
+            required: "Required",
+          })}
+          />
+          {formState.errors.firstName?.message && <StyledError>{formState.errors.firstName?.message}</StyledError>}
+      </StyledRow>
+      <StyledRow>
+        <StyledLabel htmlFor="lastName">Last name:</StyledLabel>
+        <StyledInput
+          type="text"
+          name="lastName"
+          id="last-name"
+          placeholder="Last name"
+          error={formState.errors.lastName}
+          {...register("lastName", {
+            required: "Required",
+          })}
+          />
+          {formState.errors.lastName?.message && <StyledError>{formState.errors.lastName?.message}</StyledError>}
+      </StyledRow>
+      <StyledRow>
+        <StyledLabel htmlFor="pesel">Last name:</StyledLabel>
+        <StyledInput
+          type="number"
+          name="pesel"
+          id="pesel"
+          placeholder="PESEL"
+          error={formState.errors.lastName}
+          {...register("pesel", {
+            required: "Required",
+          })}
+          />
+          {formState.errors.pesel?.message && <StyledError>{formState.errors.pesel?.message}</StyledError>}
+      </StyledRow>
+      <StyledRow>
+        <StyledLabel htmlFor="password">Last name:</StyledLabel>
+        <StyledInput
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Password"
+          error={formState.errors.lastName}
+          {...register("password", {
+            required: "Required",
+          })}
+          />
+          {formState.errors.password?.message && <StyledError>{formState.errors.password?.message}</StyledError>}
+      </StyledRow>
+      <StyledRow>
+        <StyledSubmit type="submit" value="Submit" />
+      </StyledRow>
     </StyledForm>
   );
 }
--- src/components/styles/form.js
+++ src/components/styles/form.js
@@ -4,14 +4,14 @@ export const StyledForm = styled.form`
   display: flex;
   flex-direction: column;
   max-width: 50%;
-  margin:50px auto;
+  margin: 50px auto;
 `;
 export const StyledRow = styled.div`
   width: 100%;
   margin: 0 0 20px 0;
   display: block;
-  &:last-of-type{
-      margin:0;
+  &:last-of-type {
+    margin: 0;
   }
 `;
 export const StyledLabel = styled.label`
@@ -35,27 +35,37 @@ export const StyledInput = styled.input`
   padding: 15px 10px 15px 20px;
   width: 100%;
   font-weight: 500;
+  ${({ error }) =>
+    error &&
+    `
+    border-color: red;
+  `}
+`;
+export const StyledError = styled.p`
+  display: flex;
+  justify-content: center;
+  margin: 10px 0 0;
 `;
 
 export const StyledSubmit = styled.input`
-    background: #3c3b73;
-    color: #fff;
-    height: 60px;
-    display: flex;
-    padding: 0 37px;
-    justify-content: center;
-    align-items: center;
-    cursor: pointer;
-    font-family: sans-serif;
-    font-style: normal;
-    font-weight: 500;
-    font-size: 16px;
-    transition: all .3s;
-    width: 100%;
-    border: none;
-    max-width: 300px;
-    margin: 0 auto;
-    &:hover{
-        background: #4b3fdb;
-    }
-`
+  background: #3c3b73;
+  color: #fff;
+  height: 60px;
+  display: flex;
+  padding: 0 37px;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  font-family: sans-serif;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  transition: all 0.3s;
+  width: 100%;
+  border: none;
+  max-width: 300px;
+  margin: 0 auto;
+  &:hover {
+    background: #4b3fdb;
+  }
+`;
