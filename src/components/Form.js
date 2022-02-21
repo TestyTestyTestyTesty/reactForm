13f135dc4d7f1e24787e27e0572d55ba4368d3db
STProjectPeople <tomasz.sikora@projectpeople.pl>
2022-02-21 09:40:15 +0100

    state handling

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import {
@@ -7,32 +7,68 @@ import {
   StyledLabel,
   StyledInput,
   StyledSubmit,
-  StyledError
+  StyledError,
 } from "./styles/form";
 import { yupResolver } from "@hookform/resolvers/yup";
+import axios from "axios";
 
 let schema = yup.object().shape({
-  firstName: yup.string().required('First name is required'),
-  lastName: yup.string().required('Last name is required'),
-  pesel: yup.string().required('PESEL is required').test('len', 'PESEL must be exactly 11 characters', val => val.length === 11),
-  password: yup.string().min(6).max(20).required('Password is required'),
+  firstName: yup.string().required("First name is required"),
+  lastName: yup.string().required("Last name is required"),
+  pesel: yup
+    .string()
+    .required("PESEL is required")
+    .test(
+      "len",
+      "PESEL must be exactly 11 characters",
+      (val) => val.length === 11
+    ),
+  password: yup.string().min(6).max(20).required("Password is required"),
 });
 
 export default function Form() {
+  const [countries, setCountries] = useState([]);
+  const [query, setQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
 
-  const { register, handleSubmit, formState} = useForm({
+  useEffect(() => {
+    const fetchCountries = async () => {
+      const result = await axios(
+        `https://restcountries.com/v3.1/name/${query}`
+      );
+      setCountries(result.data);
+    };
+    fetchCountries();
+  }, [query]);
+
+  const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(schema),
   });
-
+  const onChangeHandler = (value) => {
+    let matches = [];
+    setQuery(value);
+    if (value.length) {
+      matches = countries.filter(country => country.name.common.toLowerCase().indexOf(value.toLowerCase()) > -1);
+    }
+    setSuggestions(matches);
+    console.log(value, value.length, matches);
+  };
   const submitForm = (data) => {
     console.log(data);
   };
-
-/*   useEffect(() => {
-    console.log( formState);
-  },[formState]); */
   return (
     <StyledForm onSubmit={handleSubmit(submitForm)}>
+      <StyledRow>
+        <StyledLabel htmlFor="pesel">Nationality:</StyledLabel>
+        <StyledInput
+          type="search"
+          name="nationaly"
+          id="mationality"
+          placeholder="Nationality"
+          value={query}
+          onChange={(e) => onChangeHandler(e.target.value)}
+        />
+      </StyledRow>
       <StyledRow>
         <StyledLabel htmlFor="firstName">First name:</StyledLabel>
         <StyledInput
@@ -44,8 +80,10 @@ export default function Form() {
           {...register("firstName", {
             required: "Required",
           })}
-          />
-          {formState.errors.firstName?.message && <StyledError>{formState.errors.firstName?.message}</StyledError>}
+        />
+        {formState.errors.firstName?.message && (
+          <StyledError>{formState.errors.firstName?.message}</StyledError>
+        )}
       </StyledRow>
       <StyledRow>
         <StyledLabel htmlFor="lastName">Last name:</StyledLabel>
@@ -58,11 +96,13 @@ export default function Form() {
           {...register("lastName", {
             required: "Required",
           })}
-          />
-          {formState.errors.lastName?.message && <StyledError>{formState.errors.lastName?.message}</StyledError>}
+        />
+        {formState.errors.lastName?.message && (
+          <StyledError>{formState.errors.lastName?.message}</StyledError>
+        )}
       </StyledRow>
       <StyledRow>
-        <StyledLabel htmlFor="pesel">Last name:</StyledLabel>
+        <StyledLabel htmlFor="pesel">PESEL:</StyledLabel>
         <StyledInput
           type="number"
           name="pesel"
@@ -72,11 +112,14 @@ export default function Form() {
           {...register("pesel", {
             required: "Required",
           })}
-          />
-          {formState.errors.pesel?.message && <StyledError>{formState.errors.pesel?.message}</StyledError>}
+        />
+        {formState.errors.pesel?.message && (
+          <StyledError>{formState.errors.pesel?.message}</StyledError>
+        )}
       </StyledRow>
+
       <StyledRow>
-        <StyledLabel htmlFor="password">Last name:</StyledLabel>
+        <StyledLabel htmlFor="password">Password:</StyledLabel>
         <StyledInput
           type="password"
           name="password"
@@ -86,8 +129,10 @@ export default function Form() {
           {...register("password", {
             required: "Required",
           })}
-          />
-          {formState.errors.password?.message && <StyledError>{formState.errors.password?.message}</StyledError>}
+        />
+        {formState.errors.password?.message && (
+          <StyledError>{formState.errors.password?.message}</StyledError>
+        )}
       </StyledRow>
       <StyledRow>
         <StyledSubmit type="submit" value="Submit" />
